6f705342f2b760df29bfda3a7c598bd8db145ce5
aisrael <aisrael@9a779256-4b93-11de-b305-25f493754997>
2009-05-29 01:40:29 +0000

    Added ActiveTest; dirty-mockito now uses Spring (and support @Autowired).

--- src/test/java/dirty/mockito/UsesFooTest.java
+++ src/test/java/dirty/mockito/UsesFooTest.java
@@ -3,34 +3,38 @@
  *
  * This software is made available under the terms of the MIT License.
  *
- * Created May 12, 2009
+ * Created May 29, 2009
  */
 package dirty.mockito;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
 import static org.mockito.Mockito.verify;
 
 import org.junit.Test;
 import org.mockito.Mock;
 
 /**
- * JUnit test for {@link MagicMocker}.
  *
  * @author Alistair A. Israel
  */
-public class MagicMockerTest extends MagicMocker {
+public class UsesFooTest extends ActiveTest<UsesFoo> {
+
+    private UsesFoo usesFoo;
 
     @Mock
     private Foo mockFoo;
 
     /**
-     * Test for {@link MagicMocker#setupMocks()}.
+     * Test method for {@link dirty.mockito.UsesFoo#fooBar()}.
      */
     @Test
-    public final void testSetupMocks() {
+    public final void testFooBar() {
+        assertNotNull(usesFoo);
         assertNotNull(mockFoo);
-        final UsesFoo usesFoo = new UsesFoo(mockFoo);
+        assertSame(mockFoo, usesFoo.getFoo());
         usesFoo.fooBar();
         verify(mockFoo).bar();
     }
+
 }
