8fa3cf1a9ae54aed83d0c12f5abe9fe596639db0
aisrael <aisrael@9a779256-4b93-11de-b305-25f493754997>
2010-02-05 00:47:47 +0000

    [maven-release-plugin]  copy for tag dirty-mockito-0.2.4

---
+++ tags/dirty-mockito-0.2.4/src/test/java/com/example/entities/Widget.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2009 by Alistair A. Israel
+ *
+ * This software is made available under the terms of the MIT License.
+ *
+ * Created Oct 12, 2009
+ */
+package com.example.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+/**
+ * @author Alistair A. Israel
+ * @since 0.2.1
+ */
+@Entity
+@NamedQueries({ @NamedQuery(name = Widget.FIND_BY_ID, query = "SELECT w FROM Widget w WHERE w.id = :id") })
+public class Widget implements Serializable {
+
+    private static final long serialVersionUID = 4404034370024717523L;
+
+    /**
+     * {@value Widget#FIND_BY_ID}
+     */
+    public static final String FIND_BY_ID = "Widget.findById";
+
+    private Long id;
+
+    private String name;
+
+    /**
+     * @return the id
+     */
+    public final Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     *        the id to set
+     */
+    public final void setId(final Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public final String getName() {
+        return name;
+    }
+
+    /**
+     * @param name
+     *        the name to set
+     */
+    public final void setName(final String name) {
+        this.name = name;
+    }
+
+}
---
+++ tags/dirty-mockito-0.2.4/src/test/java/dirty/mockito/junit/rules/MagicMockerTest.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2009 by Alistair A. Israel
+ *
+ * This software is made available under the terms of the MIT License.
+ *
+ * Created May 12, 2009
+ */
+package dirty.mockito.junit.rules;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import com.example.Foo;
+import com.example.UsesFoo;
+
+
+/**
+ * JUnit test for {@link dirty.mockito.MagicMocker}.
+ *
+ * @author Alistair A. Israel
+ */
+public class MagicMockerTest {
+
+    /**
+     * {@link MagicMocker} does all the magic.
+     */
+    @Rule
+    // CHECKSTYLE:OFF
+    public final MagicMocker magicMocker = new MagicMocker();
+    // CHECKSTYLE:ON
+
+    @Mock
+    private Foo mockFoo;
+
+    /**
+     * Test for {@link dirty.mockito.MagicMocker#setupMocks()}.
+     */
+    @Test
+    public final void testSetupMocks() {
+        assertNotNull(mockFoo);
+        final UsesFoo usesFoo = new UsesFoo(mockFoo);
+        usesFoo.fooBar();
+        verify(mockFoo).bar();
+    }
+}
