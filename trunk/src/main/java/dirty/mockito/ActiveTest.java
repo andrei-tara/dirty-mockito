458df14b97b99bd655bebe83bcbd3556ce4442a8
aisrael <aisrael@9a779256-4b93-11de-b305-25f493754997>
2010-03-01 06:19:19 +0000

    dirty-mockito 0.3-SNAPSHOT, junit -> 4.8.1, ActiveTest now uses ActiveTestRule.

--- trunk/src/main/java/dirty/mockito/ActiveTest.java
+++ trunk/src/main/java/dirty/mockito/ActiveTest.java
@@ -10,47 +10,42 @@ package dirty.mockito;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
-import org.junit.Before;
+import org.junit.Rule;
 
 import dirty.mockito.junit.rules.ActiveTestRule;
 
 /**
  * <p>
- * JUnit 4 test parameterized base class that performs instantiation, mocking
- * and injection of mocks. Uses an internal {@link ActiveTestRule} to perform
- * mocking of elements annotated with <a href=
- * "http://mockito.googlecode.com/svn/branches/1.6/javadoc/org/mockito/Mock.html"
- * ><code>&#064;Mock</code></a> then inject those mocks into fields (in the
- * object under test) annotated with <code>&#064;Autowired</code>.
+ * JUnit 4 test parameterized base class that performs instantiation, mocking and injection of mocks. Uses an internal
+ * {@link ActiveTestRule} to perform mocking of elements annotated with <a href=
+ * "http://mockito.googlecode.com/svn/branches/1.6/javadoc/org/mockito/Mock.html" ><code>&#064;Mock</code></a> then
+ * inject those mocks into fields (in the object under test) annotated with <code>&#064;Autowired</code>.
  * </p>
  * <p>
- * Suppose we have a JPA DAO class, that depends upon an
- * <code>&#064;Autowired</code> <code>EntityManager</code>:
+ * Suppose we have a JPA DAO class, that depends upon an <code>&#064;Autowired</code> <code>EntityManager</code>:
  * </p>
- *
+ * 
  * <pre>
  * public class WidgetDao {
  *     &#064;Autowired
  *     private EntityManager em;
- *
+ * 
  *     public Widget find(final Long id) {
  *         return em.find(Widget.class, id);
  *     }
  * }
  * </pre>
  * <p>
- * Now, to write a unit test for <code>WidgetDao</code> using
- * <code>ActiveTest</code>, we simply extend
- * <code>ActiveTest&lt;WidgetDao&gt;</code>, declare a field of type
- * <code>WidgetDao</code>, declare another field of type
- * <code>Entitymanager</code> and annotate that using <code>&#064;Mock</code>.
- *
+ * Now, to write a unit test for <code>WidgetDao</code> using <code>ActiveTest</code>, we simply extend
+ * <code>ActiveTest&lt;WidgetDao&gt;</code>, declare a field of type <code>WidgetDao</code>, declare another field of
+ * type <code>Entitymanager</code> and annotate that using <code>&#064;Mock</code>.
+ * 
  * <pre>
  * public class WidgetDaoTest extends ActiveTest&lt;WidgetDao&gt; {
  *     private WidgetDao widgetDao;
  *     &#064;Mock
  *     private EntityManager em;
- *
+ * 
  *     &#064;Test
  *     public void testFind() {
  *         widgetDao.find(123L);
@@ -59,26 +54,23 @@ import dirty.mockito.junit.rules.ActiveTestRule;
  * }
  * </pre>
  * <p>
- * That's it. ActiveTest takes care of creating the mocks for fields annotated
- * <code>&#064;Mock</code>, instantiating the class under test (
- * <code>WidgetDao</code>), and injecting the mock objects into
- * <code>&#064;Autowired</code> fields.
+ * That's it. ActiveTest takes care of creating the mocks for fields annotated <code>&#064;Mock</code>, instantiating
+ * the class under test ( <code>WidgetDao</code>), and injecting the mock objects into <code>&#064;Autowired</code>
+ * fields.
  * </p>
  * <p>
- * Because Spring itself is used to inject <code>&#064;Autowired</code>
- * dependencies, <code>ActiveTest</code> supports field, setter and
- * constructor-based injection.
+ * Because Spring itself is used to inject <code>&#064;Autowired</code> dependencies, <code>ActiveTest</code> supports
+ * field, setter and constructor-based injection.
  * </p>
- *
+ * 
  * @param <T>
  *        the type of the class to instantiate and inject mocks into
  * @author Alistair A. Israel
- * @see <a href="http://code.google.com/p/dirty-mockito/" target="_blan
- *      >dirty-mockito</a>
- * @see <a href="http://static.springsource.org/spring/docs/2.5.x/api/org/springframework/beans/factory/annotation/Autowired.html"
- *      target="_blank>&#064;Autowired</a>
+ * @see <a href="http://code.google.com/p/dirty-mockito/" target="_blan >dirty-mockito</a>
  * @see <a href=
- *      "http://mockito.googlecode.com/svn/branches/1.6/javadoc/org/mockito/Mock.html"
+ *      "http://static.springsource.org/spring/docs/2.5.x/api/org/springframework/beans/factory/annotation/Autowired.html"
+ *      target="_blank>&#064;Autowired</a>
+ * @see <a href= "http://mockito.googlecode.com/svn/branches/1.6/javadoc/org/mockito/Mock.html"
  *      target="_blank>&#064;Mock</a>
  * @since 0.1
  */
@@ -87,7 +79,10 @@ public class ActiveTest<T> {
     /**
      * The {@link ActiveTestRule} that does all the mojo.
      */
-    private final ActiveTestRule<T> activeTestrule;
+    @Rule
+    // CHECKSTYLE:OFF
+    public final ActiveTestRule<T> activeTestrule;
+    // CHECKSTYLE:ON
 
     /**
      *
@@ -104,27 +99,13 @@ public class ActiveTest<T> {
     private Class<T> determineTypeParameter() {
         Class<?> specificClass = this.getClass();
         Type genericSuperclass = specificClass.getGenericSuperclass();
-        while (!(genericSuperclass instanceof ParameterizedType)
-                && specificClass != MagicTest.class) {
+        while (!(genericSuperclass instanceof ParameterizedType) && specificClass != MagicTest.class) {
             specificClass = specificClass.getSuperclass();
             genericSuperclass = specificClass.getGenericSuperclass();
         }
         final ParameterizedType parameterizedType = (ParameterizedType) genericSuperclass;
 
-        final Type firstTypeParameter = parameterizedType
-                .getActualTypeArguments()[0];
+        final Type firstTypeParameter = parameterizedType.getActualTypeArguments()[0];
         return (Class<T>) firstTypeParameter;
-    }
-
-    /**
-     * For now, we do it this way. When JUnit >= 4.7.1 comes out, we can fully
-     * make use of ActiveTestRule as a proper rule. Currently, if we use
-     * ActiveTestRule as a @Rule, then end-user @Before methods will execute
-     * <em>first</em> and any fields we're supposed to supply will be
-     * <code>null</code>.
-     */
-    @Before
-    public final void initMocks() {
-        activeTestrule.initMocks(this);
     }
 }
