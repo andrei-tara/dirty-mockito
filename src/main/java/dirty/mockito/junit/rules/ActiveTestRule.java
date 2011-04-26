27d450caa4e086953c0af9b39bd1a3d4b0c3fde2
aisrael <aisrael@gmail.com>
2011-04-26 18:56:12 +0000

    Fixed @see tag

--- src/main/java/dirty/mockito/junit/rules/ActiveTestRule.java
+++ src/main/java/dirty/mockito/junit/rules/ActiveTestRule.java
@@ -28,17 +28,14 @@ import dirty.mockito.utils.Reflection;
 
 /**
  * <p>
- * JUnit 4.7 {@link org.junit.Rule} that performs instantiation, mocking and
- * injection of mocks. Uses reflection to instantiate objects, Mockito to
- * perform mocking of elements annotated with <a href=
- * "http://mockito.googlecode.com/svn/branches/1.6/javadoc/org/mockito/Mock.html"
- * ><code>&#064;Mock</code></a>, and an internal Spring BeanFactory to perform
- * injection of mocks into fields (in the object under test) annotated with
+ * JUnit 4.7 {@link org.junit.Rule} that performs instantiation, mocking and injection of mocks. Uses reflection to
+ * instantiate objects, Mockito to perform mocking of elements annotated with <a href=
+ * "http://mockito.googlecode.com/svn/branches/1.6/javadoc/org/mockito/Mock.html" ><code>&#064;Mock</code></a>, and an
+ * internal Spring BeanFactory to perform injection of mocks into fields (in the object under test) annotated with
  * <code>&#064;Autowired</code>.
  * </p>
  * <p>
- * Suppose we have a JPA DAO class, that depends upon an
- * <code>&#064;Autowired</code> <code>EntityManager</code>:
+ * Suppose we have a JPA DAO class, that depends upon an <code>&#064;Autowired</code> <code>EntityManager</code>:
  * </p>
  *
  * <pre>
@@ -52,17 +49,14 @@ import dirty.mockito.utils.Reflection;
  * }
  * </pre>
  * <p>
- * Now, to write a unit test for <code>WidgetDao</code> using
- * <code>ActiveTestRule</code>, we simply add a {@link org.junit.Rule}
- * <code>ActiveTestRule&lt;WidgetDao&gt;</code>, declare a field of type
- * <code>WidgetDao</code>, declare another field of type
- * <code>Entitymanager</code> and annotate that using <code>&#064;Mock</code>.
+ * Now, to write a unit test for <code>WidgetDao</code> using <code>ActiveTestRule</code>, we simply add a
+ * {@link org.junit.Rule} <code>ActiveTestRule&lt;WidgetDao&gt;</code>, declare a field of type <code>WidgetDao</code>,
+ * declare another field of type <code>Entitymanager</code> and annotate that using <code>&#064;Mock</code>.
  *
  * <pre>
  * public class WidgetDaoTest {
  *     &#064;Rule
- *     public ActiveTestRule&lt;WidgetDao&gt; activeTestRule = ActiveTestRule
- *             .thatWorksOn(WidgetDao.class);
+ *     public ActiveTestRule&lt;WidgetDao&gt; activeTestRule = ActiveTestRule.thatWorksOn(WidgetDao.class);
  *
  *     &#064;Mock
  *     private EntityManager em;
@@ -77,26 +71,23 @@ import dirty.mockito.utils.Reflection;
  * }
  * </pre>
  * <p>
- * That's it. ActiveTestRule takes care of creating the mocks for fields
- * annotated <code>&#064;Mock</code>, instantiating the class under test (
- * <code>WidgetDao</code>), and injecting the mock objects into
+ * That's it. ActiveTestRule takes care of creating the mocks for fields annotated <code>&#064;Mock</code>,
+ * instantiating the class under test ( <code>WidgetDao</code>), and injecting the mock objects into
  * <code>&#064;Autowired</code> fields.
  * </p>
  * <p>
- * Because Spring itself is used to inject <code>&#064;Autowired</code>
- * dependencies, <code>ActiveTest</code> supports field, setter and
- * constructor-based injection.
+ * Because Spring itself is used to inject <code>&#064;Autowired</code> dependencies, <code>ActiveTest</code> supports
+ * field, setter and constructor-based injection.
  * </p>
  *
  * @param <T>
  *        the type (class) under test
  * @author Alistair A. Israel
- * @see <a href="http://code.google.com/p/dirty-mockito/" target="_blan
- *      >dirty-mockito</a>
- * @see <a href="http://static.springsource.org/spring/docs/2.5.x/api/org/springframework/beans/factory/annotation/Autowired.html"
- *      target="_blank>&#064;Autowired</a>
+ * @see <a href="http://code.google.com/p/dirty-mockito/" target="_blank">dirty-mockito</a>
  * @see <a href=
- *      "http://mockito.googlecode.com/svn/branches/1.6/javadoc/org/mockito/Mock.html"
+ *      "http://static.springsource.org/spring/docs/2.5.x/api/org/springframework/beans/factory/annotation/Autowired.html"
+ *      target="_blank>&#064;Autowired</a>
+ * @see <a href= "http://mockito.googlecode.com/svn/branches/1.6/javadoc/org/mockito/Mock.html"
  *      target="_blank>&#064;Mock</a>
  * @since 0.2
  */
@@ -121,8 +112,7 @@ public class ActiveTestRule<T> extends MockingRule {
      *        the class under test
      * @return an ActiveTestRule
      */
-    public static <T> ActiveTestRule<T> thatWorksOn(
-            final Class<T> classUnderTest) {
+    public static <T> ActiveTestRule<T> thatWorksOn(final Class<T> classUnderTest) {
         return new ActiveTestRule<T>(classUnderTest);
     }
 
@@ -157,20 +147,16 @@ public class ActiveTestRule<T> extends MockingRule {
      */
     @SuppressWarnings("deprecation")
     private void scan(final Object unitTest, final Class<?> clazz) {
-        final AnnotationEngine annotationEngine = new GlobalConfiguration()
-                .getAnnotationEngine();
+        final AnnotationEngine annotationEngine = new GlobalConfiguration().getAnnotationEngine();
         final Field[] fields = clazz.getDeclaredFields();
         for (final Field field : fields) {
             for (final Annotation annotation : field.getAnnotations()) {
-                final Object mock = annotationEngine.createMockFor(annotation,
-                        field);
+                final Object mock = annotationEngine.createMockFor(annotation, field);
                 if (mock != null) {
                     try {
                         Reflection.set(field).of(unitTest).to(mock);
                     } catch (final IllegalAccessException e) {
-                        throw new MockitoException(
-                                "Problems initializing fields annotated with "
-                                        + annotation, e);
+                        throw new MockitoException("Problems initializing fields annotated with " + annotation, e);
                     }
 
                     if (mock instanceof EntityManager) {
@@ -187,10 +173,8 @@ public class ActiveTestRule<T> extends MockingRule {
      *        the (mock) {@link EntityManager}
      */
     private void registerMockEntityManagerFactory(final EntityManager em) {
-        final MockEntityManagerFactory mockEntityManagerFactory = new MockEntityManagerFactory(
-                em);
-        beanFactory.registerSingleton("entityManagerFactory",
-                mockEntityManagerFactory);
+        final MockEntityManagerFactory mockEntityManagerFactory = new MockEntityManagerFactory(em);
+        beanFactory.registerSingleton("entityManagerFactory", mockEntityManagerFactory);
 
         final PersistenceAnnotationBeanPostProcessor pabpp = new PersistenceAnnotationBeanPostProcessor();
         pabpp.setBeanFactory(beanFactory);
@@ -214,14 +198,12 @@ public class ActiveTestRule<T> extends MockingRule {
                     object = beanFactory.getBean(beanName, classUnderTest);
                 } else {
                     object = (T) beanFactory.createBean(classUnderTest,
-                            AutowireCapableBeanFactory.AUTOWIRE_CONSTRUCTOR,
-                            true);
+                            AutowireCapableBeanFactory.AUTOWIRE_CONSTRUCTOR, true);
                 }
                 try {
                     Reflection.set(field).of(target).to(object);
                 } catch (final IllegalAccessException e) {
-                    throw new MockitoException(
-                            "Problems instantiating test object", e);
+                    throw new MockitoException("Problems instantiating test object", e);
                 }
             }
         }
